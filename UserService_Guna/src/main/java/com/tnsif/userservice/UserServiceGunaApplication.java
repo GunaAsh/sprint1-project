321bec80ea16c0ac5302489a8f19e3f2a2e8f635
GunaAsh <gunayow@gmail.com>
2024-04-08 18:38:29 +0530

    Its my  sprint1 project

---
+++ UserService_Guna/src/main/java/com/tnsif/userservice/UserServiceGunaApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceGunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceGunaApplication.class, args);
+	}
+
+}
